132e245809e01a12e15b27588dc9b90c9d645207
gabrielgog <gabrielgog96@gmail.com>
2024-01-15 10:47:49 +0100

    update script initialization

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import './App.css';
-import { useKlashaPayment } from './libs/klasha-script';
+import React from 'react'
 
-function App() {
-  const callWhenDone = () => {
-    console.log('call back');
-  }
-
-  const initializePayment = useKlashaPayment({
-    phone_number: '+23400000000000',
-    email: 'apps@klasha.com',
-    merchantKey: 'W2mbGtdx5vKCepFaUm2CqdzebaVW9z22shubB4xFbKTR3g4sL72+7qNQYHTUEfs0my1e/hAO1Nkdx9YbXTjUOg==',
-    amount: 1000,
-    tx_ref: 'ref',
-    businessId: 1,
-    fullname: 'Klasha Apps',
-    currency: 'NGN',
-    callback: callWhenDone,
-  });
-
+const App = () => {
   return (
-    <div className="App">
-      <button onClick={initializePayment}>
-        Pay with Klasha
-      </button>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
